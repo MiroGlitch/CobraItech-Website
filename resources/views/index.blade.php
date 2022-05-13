32c8d7b9f8e9de4fea1ddedf5e63aee15b51a587
Patrick Jerome Uy <83640317+patrickjeromeuy@users.noreply.github.com>
2022-05-13 17:33:29 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -341,25 +341,26 @@
                             </div>
 
                             <div class="col-lg-8 mt-5 mt-lg-0">
-                                <form action="" method="post" role="form" class="php-email-form">
+                                <form action="{{route('send.contact')}}" method="post" role="form" class="php-email-form">
+                                    @csrf
                                     <div class="row">
                                         <div class="col-md-6 form-group">
                                             <input type="text" name="name" class="form-control" id="name"
-                                                placeholder="Your Name" required>
+                                                placeholder="Your Name">
                                         </div>
                                         <div class="col-md-6 form-group mt-3 mt-md-0">
                                             <input type="email" class="form-control" name="email" id="email"
-                                                placeholder="Your Email" required>
+                                                placeholder="Your Email">
                                         </div>
                                     </div>
                                     <div class="form-group mt-3">
                                         <input type="text" class="form-control" name="subject" id="subject"
-                                            placeholder="Subject" required>
+                                            placeholder="Subject">
                                     </div>
                                     <div class="form-group mt-3">
-                                        <textarea class="form-control" name="message" rows="5" placeholder="Type message" required></textarea>
+                                        <textarea class="form-control" name="message" rows="5" placeholder="Type message"></textarea>
                                     </div>
-                                    <div class="text-center"><button type="submit">Send Message</button></div>
+                                    <div class="text-center"><button type="submit" id="contact-btn">Send Message</button></div>
                                 </form>
                             </div>
                         </div>
